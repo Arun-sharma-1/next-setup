5cb387d6d6afa4443b9bc8b829f5d53fb4a82176
Arun Sharma <78867795+Arun-sharma-1@users.noreply.github.com>
2025-06-14 12:48:30 +0530

    add routing and metadata

--- src/app/(protected)/member/[id]/page.tsx
+++ src/app/(protected)/member/[id]/page.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { use } from 'react'
 
+export const metadata = {
+    title: {
+        default: "Member info"
+    }
+}
+
+//async and await are used in server component , to use async operation in client component we have to use use -> use() or useserver
 const MemeberDetails = async ({ params }: { params: any }) => {
-    const { id: memberId } = await params;
-    console.log('id', memberId)
+    const data = await params;
+    // const parmas = useParams(); // to use this , we have to make this component as client 
+    // console.log('parmas', parmas)
+    console.log('id', data)
     return (
-        <div>MemeberDetails of {memberId}</div>
+        <div>MemeberDetails of</div>
     )
 }
 
--- src/app/(protected)/operation/page.tsx
+++ src/app/(protected)/operation/page.tsx
@@ -1,6 +1,8 @@
+import { rejects } from 'assert'
 import React from 'react'
 
-const OperationHomePage = () => {
+const OperationHomePage = async () => {
+    await new Promise((resolve, rejects) => { setTimeout(() => { rejects('success') }, 2000) })
     return (
         <div>OperationHomePage</div>
     )
